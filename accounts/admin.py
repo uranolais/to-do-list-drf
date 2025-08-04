18aa6b59009451722a5428e52f5d6c93d3bffe60
Lais <lais.urano@alura.com.br>
2025-08-03 21:13:33 -0300

    feat: create custom admin for users

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    Admin customizado para o modelo User seguindo o princípio de responsabilidade única.
+    Responsável apenas pela interface de administração de usuários.
+    """
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'created_at')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'created_at')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('-created_at',)
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Informações Pessoais', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissões', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Datas Importantes', {'fields': ('last_login', 'date_joined', 'created_at', 'updated_at')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+    
+    readonly_fields = ('created_at', 'updated_at')
+    
+    def get_queryset(self, request):
+        """Mostrar apenas usuários não-superusuários para administradores normais"""
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(is_superuser=False)
