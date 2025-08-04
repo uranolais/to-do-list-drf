1c729070b2e9657add468433e6a63fe4739751ab
Lais <lais.urano@alura.com.br>
2025-08-03 21:01:07 -0300

    feat: create custom User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Modelo de usuário customizado seguindo o princípio de responsabilidade única.
+    Responsável apenas por gerenciar informações do usuário.
+    """
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.username
+
